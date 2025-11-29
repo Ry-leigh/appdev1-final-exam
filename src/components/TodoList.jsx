977051bfad6ee8f358ab5e194e45fd6c2c3490a9
Ry-leigh <ryanaarondc@gmail.com>
2025-11-29 10:44:02 +0800

    Implement TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,10 +2,12 @@ import TodoItem from "./TodoItem";
 
 function TodoList({ todos }) {
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
+    <ul className="todo-list">
+      <div class="todo">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </div>
     </ul>
   );
 }
